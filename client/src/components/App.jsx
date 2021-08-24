e57d39b5fe0d80f651fff8dd1b4ab0a69062a860
Sean Park <seansangpark@gmail.com>
2021-08-24 10:14:51 -0700

    Installed necessary dependencies and completed initial client and server set-up

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Hello world from React</div>;
+  }
+}
+
+export default App;
