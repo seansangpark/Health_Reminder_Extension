a0933f49a1088b71cd32708063c9ef0133381183
Sean Park <seansangpark@gmail.com>
2021-08-25 14:57:34 -0700

    Updated with new icon and react component created

--- client/src/popup/popup.tsx
+++ client/src/popup/popup.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Reminder from './reminder';
 import './popup.css';
 
-const text = <img src='icon.png' />;
+const image = <img src='icon.png' />;
+
+const App: React.FC<{}> = () => {
+  return (
+    <div>
+      <h1>Hello from React</h1>
+      <Reminder />
+    </div>
+  );
+};
 
 const root = document.createElement('div');
 document.body.appendChild(root);
-ReactDOM.render(text, root);
+ReactDOM.render(<App />, root);
---
+++ client/src/popup/reminder.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import './popup.css';
+
+const Reminder: React.FC<{}> = () => {
+  return (
+    <div>
+      <h2>this is reminder</h2>
+    </div>
+  );
+};
+
+export default Reminder;
