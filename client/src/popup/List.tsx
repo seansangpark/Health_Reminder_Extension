3ef3b39759139136bf31727d77d27974fd544230
Sean Park <seansangpark@gmail.com>
2021-08-25 20:59:42 -0700

    Converted input field to dropdown menu

--- client/src/popup/List.tsx
+++ client/src/popup/List.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './popup.css';
 
+const timeOptions = [5, 10, 15, 20, 25, 30, 45, 60];
+
 const List: React.FC<{}> = () => {
+  const [selectedReminder, setSelectedReminder] = useState('');
+
   return (
     <form>
       <label>
         Drink Water
-        <input name='drinkWater' type='number' />
+        <select>
+          <option value='default'>Remind me every</option>
+          {timeOptions.map((time, index) => {
+            return <option value={time}>{time}</option>;
+          })}
+        </select>
       </label>
     </form>
   );
